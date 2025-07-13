2b4339c5938be3644bf851067946db06da774c25
bhawani-1405 <bhawani.khokhawat@gmail.com>
2025-07-13 22:37:28 +0530

    Update file_ops.py
    closing file and adding explaination

--- file_ops.py
+++ file_ops.py
@@ -1,5 +1,7 @@
-file = open("new_file_assignment.txt", "w")
-
+file = open("new_file_assignment.txt", "w")    #this will open/create file with given name in write mode
+#below are the lines I am writing on the file
 file.write("Hello! My name is Bhawani\n")
 file.write("I am Learning Dev-Ops from tutedude\n")
 file.write("This is assignment I am doing\n")
+
+file.close()  #file closed
