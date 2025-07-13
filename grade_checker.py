9c7bb1dc4b2419408363cf3717b20afeab45c133
bhawani-1405 <bhawani.khokhawat@gmail.com>
2025-07-13 21:56:19 +0530

    Create grade_checker.py
    all commands for assignment first

---
+++ grade_checker.py
@@ -0,0 +1,11 @@
+a = int(input("Enter your markes and get to know about your Grade: "))
+if a > 90:
+    print("Excellent! Your grade is A")
+elif a >= 80 and a < 90:
+    print("Good Job! Your grade is B")
+elif a >= 70 and a < 80:
+    print("Keep it up! Your grade is C")
+elif a >= 60 and a < 70:
+    print("You can do better! Your grade is D")
+elif a < 60:
+    print("Sorry! Your grade is F")
