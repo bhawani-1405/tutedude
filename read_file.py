e544305287e0a1e762ca2971a4b21b4b382bba25
bhawani-1405 <bhawani.khokhawat@gmail.com>
2025-07-13 22:42:28 +0530

    Update read_file.py
    add explanation

--- read_file.py
+++ read_file.py
@@ -1,4 +1,5 @@
-file = open("new_file_assignment.txt", "r")
+file = open("new_file_assignment.txt", "r")     #open the file in read mode
+#read the contents of the file and then print it. 
 contents = file.read()  
 print(contents)
-file.close()
+file.close()  #closing the file is good habit
