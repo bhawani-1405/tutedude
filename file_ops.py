4855d4b613655099112e85b53a97b4d234069b88
bhawani-1405 <bhawani.khokhawat@gmail.com>
2025-07-13 22:35:30 +0530

    Create file_ops.py
    file_ops

---
+++ file_ops.py
@@ -0,0 +1,5 @@
+file = open("new_file_assignment.txt", "w")
+
+file.write("Hello! My name is Bhawani\n")
+file.write("I am Learning Dev-Ops from tutedude\n")
+file.write("This is assignment I am doing\n")
